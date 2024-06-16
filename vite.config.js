ed9f04815f1ec446dc5c2df0150a94114296a3f5
AvitalHamburg <choenmatilda@gmail.com>
2024-06-16 10:05:16 +0300

    git commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import FontsPlugin from 'unplugin-fonts/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(), // Vue plugin
+    FontsPlugin({
+      // Configuration options for unplugin-fonts
+      extensions: ['woff', 'woff2'], // File extensions to include
+      limit: 4096, // Limit in bytes for inlining fonts (optional)
+      fontFace: true, // Generate @font-face rules (optional)
+      preload: true, // Add preload links to fonts (optional)
+    }),
+  ],
+});
