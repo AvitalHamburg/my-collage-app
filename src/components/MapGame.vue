9c9d03314b003e09f34922e749089235f6bc045c
AvitalHamburg <choenmatilda@gmail.com>
2024-06-24 09:35:19 +0300

    git commit-m "09:35"

--- src/components/MapGame.vue
+++ src/components/MapGame.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="page">
-      <img  class=game-board src="../assets/imgs/collageMap.png">
+      <img class="game-board" src="../assets/imgs/collageMap.jpeg">
       <div class="container">
         <div
           v-for="index in 8"
@@ -10,6 +10,9 @@
           draggable="true"
           @dragstart="dragStart"
           @dragend="dragEnd"
+          @touchstart="touchStart"
+          @touchend="touchEnd"
+          @touchmove="touchMove"
         >
           Item {{ index }}
         </div>
@@ -26,7 +29,8 @@
           @dragenter="dragEnter"
           @dragleave="dragLeave"
         >
-      </div>
+          <!-- Adjust content as needed -->
+        </div>
       </div>
     </div>
   </template>
@@ -35,11 +39,31 @@
   export default {
     methods: {
       dragStart(event) {
-        // Store the dragged element's ID in data transfer
         event.dataTransfer.setData('text/plain', event.target.id);
         event.target.classList.add('dragging');
       },
       dragEnd(event) {
+        event.target.classList.remove('dragging');
+      },
+      touchStart(event) {
+        // Store initial touch position
+        this.startX = event.touches[0].clientX - event.target.getBoundingClientRect().left;
+        this.startY = event.touches[0].clientY - event.target.getBoundingClientRect().top;
+        
+        // Store the dragged element's ID in data transfer
+        event.dataTransfer.setData('text/plain', event.target.id);
+        event.target.classList.add('dragging');
+      },
+      touchMove(event) {
+        // Calculate new position based on touch movement
+        const offsetX = event.touches[0].clientX - this.startX;
+        const offsetY = event.touches[0].clientY - this.startY;
+        
+        // Apply the new position to the dragged element
+        event.target.style.left = offsetX + 'px';
+        event.target.style.top = offsetY + 'px';
+      },
+      touchEnd(event) {
         event.target.classList.remove('dragging');
       },
       drop(event) {
@@ -48,12 +72,8 @@
         const draggedItem = document.getElementById(itemId);
         const dropZoneId = event.target.id;
         
-        // Check if the dragged item can be dropped into the target container
         if (itemId === `item${dropZoneId.slice(-1)}`) {
-          // Remove the element from its original parent
           draggedItem.parentNode.removeChild(draggedItem);
-          
-          // Append the element to the drop zone
           event.target.appendChild(draggedItem);
         }
       },
@@ -68,25 +88,27 @@
   </script>
   
   <style scoped>
-  #page{
+  #page {
     background-color: aliceblue;
-    height: 100vh;width: 100vw;
+    height: 100vh;
+    width: 100vw;
     position: absolute;
-    top:0;
+    top: 0;
     left: 0;
   }
-  .game-board{
-    width:100vw;
-    height:auto;
-    position: absolute;
-    z-index:0;
-    right:0;
-    top:30%;
   
+  .game-board {
+    width: 100%;
+    height: auto;
+    position: absolute;
+    z-index: 0;
+    right: 0;
+    top: 30%;
   }
+  
   .draggable-item {
     padding: 5px;
-    background-color:rgb(89,89,89);
+    background-color: rgb(89, 89, 89);
     border: 1px solid #0d0d0d;
     border-radius: 500px;
     width: 15vw;
@@ -101,11 +123,8 @@
     border: 2px dashed black;
     border-radius: 500px;
     width: 20vw;
-    height:3vh;
+    height: 3vh;
     cursor: pointer;
-    align-items:center;
-  
-    
   }
   
   .dragging {
@@ -115,41 +134,58 @@
   .dragover {
     background-color: #c0c0c0;
   }
-  .container{
-    position:absolute;
-    left:6%;
-    top:57%;
-    display:flex;
-    flex-wrap:wrap;
-    gap: 5%;
-    z-index:10;
-    justify-content:center;
   
-  }
-  .container1{
+  .container {
     position: absolute;
-    left:30%;
-    top:20%;
-    z-index:10;
-  
-  
+    left: 6%;
+    top: 25%;
+    gap: 5%;
+    z-index: 10;
+    justify-content: center;
   }
-  #container1{
-    position:absolute;
-    right:-140%;
-    top:-20%;
+  
+  .container1 {
+    position: absolute;
+    left: 30%;
+    top: 20%;
+    z-index: 10;
+  }
+  
+  @media screen and (max-width: 768px) {
+    /* Additional adjustments for smaller screens */
+    .container {
+    position: absolute;
+    left: 2%;
+    top: 20%;
+    gap: 5%;
+    z-index: 10;
     
   }
-  #container2{
-    position:absolute;
-    right:210%;
-    top:45%;
-  }
-  #container3{
-    position:absolute;
-    right:-50%;
-    top:35%;
+  
+    .game-board {
+      top: 20%;
+      width: 100%;
+      height: auto;
+      position: absolute;
+      z-index: 0;
+      right: 0;
+    }
+  
+    .dropzone {
+      width: 35vw;
+      height: 5vh;
+    }
+  
+    .draggable-item {
+      width: 30vw;
+    }
+    #container1{
+      position:absolute;
+      left:80%;
+      top:25%;
+    }
+  
+  
   }
   </style>
-  
   
